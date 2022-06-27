985463cc455ee52ad99c0d95910d65e64b67d174
Gabriel Brizuela <fgb.8513@gmail.com>
2022-06-26 23:22:19 -0300

    agrego contenido

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("hola mundo")
+let a = 13
+let b = 9
+ console.log (`La variable tiene un valor de ${a}`)
+
+ for (let i = 0;i<=10;i++ ) {
+     alert (`El valor de I es ${i}`);
+     console.log (`El valor de I es ${i}`)
+ }
